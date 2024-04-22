f717bf189b320284195ad6edabc62e1b84235d6c
Tomas Marquez <btomasmarquez@gmail.com>
2024-04-22 08:52:27 -0300

    added .env file

--- myApp.js
+++ myApp.js
@@ -1,10 +1,21 @@
 var express = require('express');
 var app = express();
 var port = 5050;
+var dotenv = require('dotenv').config()
 
 app.listen(port, () => {
     console.log("Hello World")
   });
+
+app.get("/json", (req, res) => {
+  var response = process.env.MESSAGE_STYLE
+
+if (response === 'uppercase'){
+  return res.json('HELLO JSON')
+} else {
+  return res.json('Hello json')
+} 
+})
 
 
 
